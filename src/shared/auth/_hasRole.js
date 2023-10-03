e301f99ef502a6ad93c4fd1e4cf918453cb12d81
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 17:44:25 +0500

    ro'yxatdan o'tgan userni rollarini tekshirib olish uchun middleware yasaldi.

---
+++ src/shared/auth/_hasRole.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { ForbiddenError } = require("../errors");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
+const hasRole = (roles) => {
+  return async (req, res, next) => {
+    try {
+      const { role } = req.user;
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+module.exports = hasRole;
