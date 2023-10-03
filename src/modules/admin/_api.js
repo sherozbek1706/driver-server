b1366ed15685bf191822ccc5e71bf8ba1e1cdc27
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 18:09:44 +0500

    admin qo'shish uchun router

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -1,7 +1,12 @@
-const { loginAdmin } = require("./_controller");
+const { loginAdmin, addAdmin } = require("./_controller");
+const { hasRole, isLoggedIn } = require("../../shared/auth");
+const { admin_img_upload } = require("../../shared/upload");
 
 const router = require("express").Router();
 
+const mAddAdmin = [admin_img_upload, isLoggedIn, hasRole(["super_admin"])];
+
 router.post("/admin/login", loginAdmin);
+router.post("/admin", mAddAdmin, addAdmin);
 
 module.exports = router;
