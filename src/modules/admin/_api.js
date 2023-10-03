e0a2c7bac6205c6448d95aea5d7c2a5ea7bcff7e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 20:02:41 +0500

    show qilish uchun router kerak

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -1,4 +1,4 @@
-const { loginAdmin, addAdmin, listAdmin } = require("./_controller");
+const { loginAdmin, addAdmin, listAdmin, showAdmin } = require("./_controller");
 const { hasRole, isLoggedIn } = require("../../shared/auth");
 const { admin_img_upload } = require("../../shared/upload");
 
@@ -6,9 +6,15 @@ const router = require("express").Router();
 
 const mAddAdmin = [admin_img_upload, isLoggedIn, hasRole(["super_admin"])];
 const mListAdmin = [admin_img_upload, isLoggedIn, hasRole(["super_admin"])];
+const mShowAdmin = [
+  admin_img_upload,
+  isLoggedIn,
+  hasRole(["super_admin", "admin"]),
+];
 
 router.post("/admin/login", loginAdmin);
 router.post("/admin", mAddAdmin, addAdmin);
 router.get("/admin", mListAdmin, listAdmin);
+router.get("/admin/:id", mShowAdmin, showAdmin);
 
 module.exports = router;
