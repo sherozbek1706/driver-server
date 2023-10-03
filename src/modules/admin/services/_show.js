2c578e8ce222e33e8d628b06d69da06b729acbca
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 20:04:23 +0500

    services ochib olinsin.

---
+++ src/modules/admin/services/_show.js
@@ -0,0 +1,7 @@
+const db = require("../../../db");
+const { ForbiddenError, NotFoundError } = require("../../../shared/errors");
+
+const show = async ({ params, user, role }) => {
+};
+
+module.exports = show;
--- src/modules/admin/services/index.js
+++ src/modules/admin/services/index.js
@@ -1,5 +1,6 @@
 const login = require("./_login");
 const add = require("./_add");
 const list = require("./_list");
+const show = require("./_show");
 
-module.exports = { login, add, list };
+module.exports = { login, add, list, show };
