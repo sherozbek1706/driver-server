519380e78040decc5b9d1bd18b125abee9b9d2d6
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 18:00:29 +0500

    Agar telefon raqam oldinishlatilgan bo'lsa xato qaytarib yubor.

--- src/modules/admin/services/_add.js
+++ src/modules/admin/services/_add.js
@@ -3,9 +3,15 @@ const { BadRequestError } = require("../../../shared/errors");
 const bcryptjs = require("bcryptjs");
 
 const add = async ({ body, image }) => {
+  const { phone_number, username, password } = body;
 
+  let existing;
 
+  existing = await db("admin").where({ username }).first();
 
+  if (existing) {
+    throw new BadRequestError("Username oldin ro'yxatdan o'tgan!");
+  }
 
 
 
