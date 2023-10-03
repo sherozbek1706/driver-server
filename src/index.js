4235f4f8b26d08d331877c2a2497788d10cd531b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 14:55:54 +0500

    Error middlewareni ulab register qil.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// import handleError
+const handleError = require("./shared/errors/handle");
+
+
+// registered Error
+app.use(handleError);
+
 app.listen(config.port || 5001, () => {
   console.log(`SERVER HAS BEEN STARTED ON LOCAL PORT ${config.port || 5001}`);
   console.log(
