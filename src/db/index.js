752b1055596038b2ddca3b889b6c91eab05ada73
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 15:13:42 +0500

    Databazaga ulanib olish.

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+const knex = require("knex");
+const config = require("../shared/config");
+const db = knex({
+  client: "postgresql",
+  connection: {
+    database: config.db.database,
+    user: config.db.username,
+    password: config.db.password,
+    port: config.db.port,
+  },
+});
+
+module.exports = db;
