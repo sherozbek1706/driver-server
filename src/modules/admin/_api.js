62915f4de54171a947594d0be5b6498dca1ab3b3
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 19:02:59 +0500

    Admin listni olish uchun router yasalsin.

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -1,12 +1,14 @@
-const { loginAdmin, addAdmin } = require("./_controller");
+const { loginAdmin, addAdmin, listAdmin } = require("./_controller");
 const { hasRole, isLoggedIn } = require("../../shared/auth");
 const { admin_img_upload } = require("../../shared/upload");
 
 const router = require("express").Router();
 
 const mAddAdmin = [admin_img_upload, isLoggedIn, hasRole(["super_admin"])];
+const mListAdmin = [admin_img_upload, isLoggedIn, hasRole(["super_admin"])];
 
 router.post("/admin/login", loginAdmin);
 router.post("/admin", mAddAdmin, addAdmin);
+router.get("/admin", mListAdmin, listAdmin);
 
 module.exports = router;
