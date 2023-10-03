3289338b9c60e4d0485e85f24acb0870bc0b2628
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 18:01:43 +0500

    username oldin ruyxatdan o'tgan bo'lsa error qaytarib yubor.

--- src/modules/admin/services/_add.js
+++ src/modules/admin/services/_add.js
@@ -13,9 +13,17 @@ const add = async ({ body, image }) => {
     throw new BadRequestError("Username oldin ro'yxatdan o'tgan!");
   }
 
+  existing = await db("admin").where({ phone_number }).first();
 
+  if (existing) {
+    throw new BadRequestError("Telefon raqam oldin ro'yxatdan o'tgan!");
+  }
 
+  const hashed_psw = await bcryptjs.hash(password, 10);
 
+  return db("admin")
+    .insert({ ...body, image, password: hashed_psw })
+    .returning("*");
 };
 
 module.exports = add;
