751ef84fec5080c969fd3e044b65bd83cdcd6bc7
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 15:10:59 +0500

    login qilish uchun service yasab olinsin.

---
+++ src/modules/admin/services/_login.js
@@ -0,0 +1,14 @@
+const db = require("../../../db");
+const { NotFoundError, ForbiddenError } = require("../../../shared/errors");
+const jsonwebtoken = require("jsonwebtoken");
+const config = require("../../../shared/config");
+
+const login = async ({ body }) => {
+
+
+
+
+
+};
+
+module.exports = login;
