d5cde13c0f4c6fb6e90b90cd3815ef296e1efbef
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 20:05:06 +0500

    agar admin topilmasa erro xabar berilsin.

--- src/modules/admin/services/_show.js
+++ src/modules/admin/services/_show.js
@@ -2,6 +2,14 @@ const db = require("../../../db");
 const { ForbiddenError, NotFoundError } = require("../../../shared/errors");
 
 const show = async ({ params, user, role }) => {
+  let existing = await db("admin")
+    .where({ id: params.id, is_deleted: false })
+    .first();
+
+  if (!existing) {
+    throw new NotFoundError("Admin topilmadi!");
+  }
+
 };
 
 module.exports = show;
