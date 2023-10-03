9a776def7bb644b8c026a8e20bf17c81fa9fa13b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-03 17:44:56 +0500

    agar role xato bo'lsa xabar keladi.

--- src/shared/auth/_hasRole.js
+++ src/shared/auth/_hasRole.js
@@ -13,6 +13,10 @@ const hasRole = (roles) => {
     try {
       const { role } = req.user;
 
+      if (!roles.includes(role)) {
+        throw new ForbiddenError("Sizda bunday huquq yo'q!");
+      }
+
       next();
     } catch (error) {
       next(error);
