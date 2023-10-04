7027d932ced6dae62baa99117e7b8a90f0850e78
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-04 09:05:14 +0500

    remove admin qilish uchun controllerni routerga ulash kerak.

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -1,9 +1,16 @@
-const { loginAdmin, addAdmin, listAdmin, showAdmin } = require("./_controller");
+const {
+  loginAdmin,
+  addAdmin,
+  listAdmin,
+  showAdmin,
+  removeAdmin,
+} = require("./_controller");
 const { hasRole, isLoggedIn } = require("../../shared/auth");
 const { admin_img_upload } = require("../../shared/upload");
 
 const router = require("express").Router();
 
+const mLoginAdmin = [admin_img_upload];
 const mAddAdmin = [admin_img_upload, isLoggedIn, hasRole(["super_admin"])];
 const mListAdmin = [admin_img_upload, isLoggedIn, hasRole(["super_admin"])];
 const mShowAdmin = [
@@ -11,10 +18,12 @@ const mShowAdmin = [
   isLoggedIn,
   hasRole(["super_admin", "admin"]),
 ];
+const mRemoveAdmin = [admin_img_upload, isLoggedIn, hasRole(["super_admin"])];
 
-router.post("/admin/login", loginAdmin);
+router.post("/admin/login", mLoginAdmin, loginAdmin);
 router.post("/admin", mAddAdmin, addAdmin);
 router.get("/admin", mListAdmin, listAdmin);
 router.get("/admin/:id", mShowAdmin, showAdmin);
+router.delete("/admin/:id", mRemoveAdmin, removeAdmin);
 
 module.exports = router;
