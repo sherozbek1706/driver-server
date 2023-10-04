ef587b54a672b7f1a4184d2a86fb2969d222c229
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-04 09:16:38 +0500

    adminni qayta tiklash uchun service

---
+++ src/modules/admin/services/_unremove.js
@@ -0,0 +1,5 @@
+const db = require("../../../db");
+const { NotFoundError, ForbiddenError } = require("../../../shared/errors");
+
+const unremove = async ({ params, user }) => {
+module.exports = unremove;
--- src/modules/admin/services/index.js
+++ src/modules/admin/services/index.js
@@ -3,5 +3,6 @@ const add = require("./_add");
 const list = require("./_list");
 const show = require("./_show");
 const remove = require("./_remove");
+const unremove = require("./_unremove");
 
-module.exports = { login, add, list, show, remove };
+module.exports = { login, add, list, show, remove, unremove };
