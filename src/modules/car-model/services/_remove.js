7f304decf0b98c83d0451469d035d0a3fadc67d5
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-04 17:21:58 +0500

    agar topilmaqsa xato

--- src/modules/car-model/services/_remove.js
+++ src/modules/car-model/services/_remove.js
@@ -2,6 +2,11 @@ const db = require("../../../db");
 const { NotFoundError } = require("../../../shared/errors");
 
 const remove = async ({ params }) => {
+  const existing = await db("car_model").where({ id: params.id }).first();
+
+  if (!existing) {
+    throw new NotFoundError("Car Model topilmadi!");
+  }
 
   return db("car_model").where({ id: params.id }).del().returning("*");
 };
