73ebcb44c117c1e519aeb7c60a456a5b0f243704
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-04 09:17:07 +0500

    agar o'chirilgan admin topilmasa "admin topilmadi!" - deydi.

--- src/modules/admin/services/_unremove.js
+++ src/modules/admin/services/_unremove.js
@@ -2,4 +2,12 @@ const db = require("../../../db");
 const { NotFoundError, ForbiddenError } = require("../../../shared/errors");
 
 const unremove = async ({ params, user }) => {
+  const existing = await db("admin")
+    .where({ ...params, is_deleted: true })
+    .first();
+
+  if (!existing) {
+    throw new NotFoundError("Admin topilmadi!");
+  }
+
 module.exports = unremove;
