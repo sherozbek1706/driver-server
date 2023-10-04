3bffec63179ff9d9cc6e696c1085c662808e3f4c
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-04 09:06:29 +0500

    agar admin topilmasa xatolikni xabar ko'rinishida chiqar

--- src/modules/admin/services/_remove.js
+++ src/modules/admin/services/_remove.js
@@ -2,4 +2,12 @@ const db = require("../../../db");
 const { NotFoundError, ForbiddenError } = require("../../../shared/errors");
 
 const remove = async ({ params, user }) => {
+  const existing = await db("admin")
+    .where({ ...params, is_deleted: false })
+    .first();
+
+  if (!existing) {
+    throw new NotFoundError("Admin topilmadi!");
+  }
+
 module.exports = remove;
