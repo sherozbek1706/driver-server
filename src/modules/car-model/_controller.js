83df0b5cf795669991eae85687d76353d97442e0
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-04 17:18:54 +0500

    carmodel uchun controller ochilsin

---
+++ src/modules/car-model/_controller.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { add, list, remove } = require("./services");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const addCarModel = async (req, res, next) => {
+  try {
+    const result = await add({ body: req.body });
+    res.status(201).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
