27beab811ec9c4afd4a6eac25e5155531996047e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-04 09:06:04 +0500

    remove admin uchun service yasab ol.

---
+++ src/modules/admin/services/_remove.js
@@ -0,0 +1,5 @@
+const db = require("../../../db");
+const { NotFoundError, ForbiddenError } = require("../../../shared/errors");
+
+const remove = async ({ params, user }) => {
+module.exports = remove;
--- src/modules/admin/services/index.js
+++ src/modules/admin/services/index.js
@@ -2,5 +2,6 @@ const login = require("./_login");
 const add = require("./_add");
 const list = require("./_list");
 const show = require("./_show");
+const remove = require("./_remove");
 
-module.exports = { login, add, list, show };
+module.exports = { login, add, list, show, remove };
