57e4380fa4ca8e726cafb3ef3f49f1b6d6cf2b67
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-05 10:11:03 +0500

    car-region uchun migrations

---
+++ migrations/2_car-region.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("car_region", function (table) {
+    table.increments("id").primary();
+    table.string("region").notNullable();
+    table.string("number").notNullable().unique();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("car_region");
+};
