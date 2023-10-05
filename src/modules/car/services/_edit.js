8f370bb03d931442343e7010a9780f16e0154f86
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-05 18:36:15 +0500

    params bo'yicha moshina topilsa xato

--- src/modules/car/services/_edit.js
+++ src/modules/car/services/_edit.js
@@ -2,5 +2,11 @@ const db = require("../../../db");
 const { NotFoundError, BadRequestError } = require("../../../shared/errors");
 
 const edit = async ({ params, body }) => {
+  const exist = await db("car").where({ id: params.id });
+
+  if (!exist) {
+    throw new NotFoundError("Moshina topilmadi!");
+  }
+
 };
 module.exports = edit;
