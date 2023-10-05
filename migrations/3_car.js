4b784cdde811b23f47cefed72cfcc770cbad2ed7
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-05 12:51:13 +0500

    Mashina uchun migration ochib oldim.

---
+++ migrations/3_car.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("car", (table) => {
+    table.increments("id").primary();
+    table.integer("region_id").references("id").inTable("car_region");
+    table.integer("model_id").references("id").inTable("car_model");
+    table.string("number").notNullable().unique();
+    table.integer("year").notNullable();
+    table.string("color").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropSchema("car");
+};
