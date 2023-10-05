6224608191426dad11ba462ce780cdc0dc2bcc12
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-05 18:35:22 +0500

    edit qilish uchun services ochilishi kerak!

---
+++ src/modules/car/services/_edit.js
@@ -0,0 +1,6 @@
+const db = require("../../../db");
+const { NotFoundError, BadRequestError } = require("../../../shared/errors");
+
+const edit = async ({ params, body }) => {
+};
+module.exports = edit;
