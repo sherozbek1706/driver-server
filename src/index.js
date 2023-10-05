9a9ee81d91ba46c91d283a780318023d108ab1a2
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-05 10:12:05 +0500

    car region route'ni index'ga uladim.

--- src/index.js
+++ src/index.js
@@ -14,10 +14,12 @@ const handleError = require("./shared/errors/handle");
 // import router
 const admin_route = require("./modules/admin/_api");
 const carmodel_route = require("./modules/car-model/_api");
+const carregion_route = require("./modules/car-region/_api");
 
 // registered router
 app.use(admin_route);
 app.use(carmodel_route);
+app.use(carregion_route);
 
 // registered Error
 app.use(handleError);
