216c6159376eb100494095179b4ca434e11d4878
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-05 11:07:22 +0500

    edit qilish uchun services ochish

---
+++ src/modules/car-region/services/_edit.js
@@ -0,0 +1,6 @@
+const db = require("../../../db");
+const { BadRequestError, NotFoundError } = require("../../../shared/errors");
+
+const edit = async ({ params, body }) => {
+
+module.exports = edit;
