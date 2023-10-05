42cfc47439634b3bae2356be152e4d030f43d684
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-05 12:53:44 +0500

    Berilishi bilan model qidiriladi. Agar topilmasa xate beradi.

--- src/modules/car/services/_add.js
+++ src/modules/car/services/_add.js
@@ -2,6 +2,15 @@ const db = require("../../../db");
 const { NotFoundError, BadRequestError } = require("../../../shared/errors");
 
 const add = async ({ body }) => {
+  const { model_id, region_id, number } = body;
+  let existing;
+
+  existing = await db("car_model").where({ id: model_id }).first();
+
+  if (!existing) {
+    throw new NotFoundError("Model topilmadi!");
+  }
+
 };
 
 module.exports = add;
