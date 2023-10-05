d8ef445ce3535891920c719bc3d81a6a0e61dd46
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-05 13:00:17 +0500

    listni olish uchun controller yasaldi.

--- src/modules/car/_controller.js
+++ src/modules/car/_controller.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { add } = require("./services");
+const { add, list } = require("./services");
 
 /**
  *
@@ -16,6 +16,22 @@ const addCar = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const listCar = async (req, res, next) => {
+  try {
+    const result = await list();
+    res.status("200").json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   addCar,
+  listCar,
 };
