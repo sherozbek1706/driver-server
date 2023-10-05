d75ed652e04cc75b9c58aa2f7bbdbf9ca5f3f2ad
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-05 11:07:40 +0500

    car region topilmasa error

--- src/modules/car-region/services/_edit.js
+++ src/modules/car-region/services/_edit.js
@@ -2,5 +2,11 @@ const db = require("../../../db");
 const { BadRequestError, NotFoundError } = require("../../../shared/errors");
 
 const edit = async ({ params, body }) => {
+  const existing = await db("car_region").where({ id: params.id });
+
+  if (!existing) {
+    throw new NotFoundError("Car Region topilmadi!");
+  }
+
 
 module.exports = edit;
