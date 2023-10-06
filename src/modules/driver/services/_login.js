9fc7e49339415bcea5e1bf4c82cbc34c794363be
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 11:46:03 +0500

    agar username xato bo'lsa aytadi

--- src/modules/driver/services/_login.js
+++ src/modules/driver/services/_login.js
@@ -5,6 +5,14 @@ const config = require("../../../shared/config");
 const bcryptjs = require("bcryptjs");
 
 const login = async ({ body }) => {
+  const { username, password } = body;
+  const existing = await db("driver")
+    .where({ username, is_deleted: false })
+    .first();
+
+  if (!existing) {
+    throw new NotFoundError("Username xato kiritildi!");
+  }
 };
 
 module.exports = login;
