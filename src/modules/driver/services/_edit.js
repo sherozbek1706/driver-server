7840d84a5939d3227fc8d4b19b8301a63866b452
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 21:25:28 +0500

    username tekshirish

--- src/modules/driver/services/_edit.js
+++ src/modules/driver/services/_edit.js
@@ -21,6 +21,21 @@ const edit = async ({ params, user, body, image }) => {
     }
   }
 
+  // haydovchi yasab boraman birdaniga
+  let driver = {};
+
+  // agar oldin ro'yxatdan o'tgan bo'lsa elon qilaman.
+  let existed;
+
+  // tekshirib olish uchun ishlatdim.
+  const { phone_number, username, car_id, password, ...data } = body;
+
+  existed = await db("driver").where({ username }).first();
+
+  if (existed && existed.username != existing.username) {
+    throw new BadRequestError("Username oldin ro'yxatdan o'tgan!");
+  }
+
 };
 
 module.exports = edit;
