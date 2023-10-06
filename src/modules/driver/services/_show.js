46800c1a339c26b7e39ba0e8666d0f22e3b26748
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 11:38:49 +0500

    Agar qidirilganda topilmasa xato beradi.

--- src/modules/driver/services/_show.js
+++ src/modules/driver/services/_show.js
@@ -2,6 +2,14 @@ const db = require("../../../db");
 const { ForbiddenError, NotFoundError } = require("../../../shared/errors");
 
 const show = async ({ params, user, role }) => {
+  let existing = await db("driver")
+    .where({ id: params.id, is_deleted: false })
+    .first();
+
+  if (!existing) {
+    throw new NotFoundError("Haydovchi topilmadi!");
+  }
+
   return existing;
 };
 
