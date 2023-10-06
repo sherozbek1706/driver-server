a5a2092ff9b311a1a77c0e02dc8a61287163a816
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 11:38:33 +0500

    haydovchilarni id bo'yicha qidirib topish

---
+++ src/modules/driver/services/_show.js
@@ -0,0 +1,8 @@
+const db = require("../../../db");
+const { ForbiddenError, NotFoundError } = require("../../../shared/errors");
+
+const show = async ({ params, user, role }) => {
+  return existing;
+};
+
+module.exports = show;
--- src/modules/driver/services/index.js
+++ src/modules/driver/services/index.js
@@ -1,7 +1,10 @@
 const add = require("./_add");
 const list = require("./_list");
+const show = require("./_show");
 
 module.exports = {
   add,
   list,
+  show
+
 };
