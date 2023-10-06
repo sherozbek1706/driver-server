61f27fa99172108dd244815f0ad62dc602b37387
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 17:37:00 +0500

    agar haydovchi topilmasa xato chiqaradi

--- src/modules/driver/services/_off-active.js
+++ src/modules/driver/services/_off-active.js
@@ -2,6 +2,14 @@ const db = require("../../../db");
 const { ForbiddenError, NotFoundError } = require("../../../shared/errors");
 
 const offActive = async ({ params }) => {
+  let existing = await db("driver")
+    .where({ id: params.id, is_deleted: false })
+    .first();
+
+  if (!existing) {
+    throw new NotFoundError("Haydovchi topilmadi!");
+  }
+
   return db("driver")
     .where({ id: params.id })
     .update({ active: false })
