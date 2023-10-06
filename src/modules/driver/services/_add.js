c2dc20c0cd6113f8ede4c75c68d3bd3c531811b1
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 11:09:22 +0500

    haydovchini qo'shish uchun services

---
+++ src/modules/driver/services/_add.js
@@ -0,0 +1,8 @@
+const db = require("../../../db");
+const { BadRequestError } = require("../../../shared/errors");
+const bcryptjs = require("bcryptjs");
+
+const add = async ({ body, image, user }) => {
+};
+
+module.exports = add;
