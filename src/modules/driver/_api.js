d8f9138ca1183d281ff21c559a8aaf11b069803f
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 11:46:55 +0500

    alohida login qilish uchun route

--- src/modules/driver/_api.js
+++ src/modules/driver/_api.js
@@ -1,4 +1,9 @@
-const { addDriver, listDriver, showDriver } = require("./_controller");
+const {
+  addDriver,
+  listDriver,
+  showDriver,
+  loginDriver,
+} = require("./_controller");
 
 const { hasRole, isLoggedIn } = require("../../shared/auth");
 const { driver_img_upload } = require("../../shared/upload");
@@ -6,6 +11,7 @@ const { isBlock } = require("../admin/middleware");
 
 const router = require("express").Router();
 
+const mLoginDriver = [driver_img_upload];
 const mAddDriver = [
   driver_img_upload,
   isLoggedIn,
@@ -22,9 +28,10 @@ const mShowDriver = [
   driver_img_upload,
   isLoggedIn,
   hasRole(["admin", "super_admin", "driver"]),
-  isBlock,
+  // isBlock,
 ];
 
+router.post("/driver/login", mLoginDriver, loginDriver);
 router.post("/driver/add", mAddDriver, addDriver);
 router.get("/driver", mListDriver, listDriver);
 router.get("/driver/:id", mShowDriver, showDriver);
