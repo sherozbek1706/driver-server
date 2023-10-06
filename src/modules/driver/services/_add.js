6109830acfe6857a77b2c179cd6f98c969b4403b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 11:10:25 +0500

    agar telefon raqam oldin ro'yxatdan o'tgan bo'lsa messagega e'lon ber

--- src/modules/driver/services/_add.js
+++ src/modules/driver/services/_add.js
@@ -3,6 +3,16 @@ const { BadRequestError } = require("../../../shared/errors");
 const bcryptjs = require("bcryptjs");
 
 const add = async ({ body, image, user }) => {
+  const { phone_number, username, password } = body;
+
+  let existing;
+
+  existing = await db("driver").where({ username }).first();
+
+  if (existing) {
+    throw new BadRequestError("Username oldin ro'yxatdan o'tgan!");
+  }
+
 };
 
 module.exports = add;
