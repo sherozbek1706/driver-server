06cb4fb9f3a705533291e50d13abe2bf27508808
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 11:32:39 +0500

    haydovchilarni listni ko'rish uchun controller.

--- src/modules/driver/_controller.js
+++ src/modules/driver/_controller.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { add } = require("./services");
+const { add, list } = require("./services");
 
 /**
  *
@@ -12,7 +12,7 @@ const addDriver = async (req, res, next) => {
     const result = await add({
       body: req.body,
       image: `/files/driver/${req.file.filename}`,
-      user: req.user
+      user: req.user,
     });
     res.status(201).json({ data: result });
   } catch (error) {
@@ -20,6 +20,22 @@ const addDriver = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const listDriver = async (req, res, next) => {
+  try {
+    const result = await list();
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   addDriver,
+  listDriver,
 };
