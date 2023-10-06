0350d4cfec736eda1995cc2c24a5e2fc9d84eaa8
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 21:24:56 +0500

    agar id bo'yicha haydovchi topilmsa xatolik beradi

--- src/modules/driver/services/_edit.js
+++ src/modules/driver/services/_edit.js
@@ -7,6 +7,14 @@ const {
 const bcryptjs = require("bcryptjs");
 
 const edit = async ({ params, user, body, image }) => {
+  let existing = await db("driver")
+    .where({ id: params.id, is_deleted: false })
+    .first();
+
+  if (!existing) {
+    throw new NotFoundError("Haydovchi topilmadi!");
+  }
+
 };
 
 module.exports = edit;
