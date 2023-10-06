b5e23289aba2291dfa0a5eb84fbac33ad1740404
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-06 15:42:54 +0500

    agar so'rov jo'natgan user admin bo'lsa unda sakrab o'tib ketadi.

--- src/modules/driver/middleware/_is-active.js
+++ src/modules/driver/middleware/_is-active.js
@@ -3,6 +3,10 @@ const { ForbiddenError, NotFoundError } = require("../../../shared/errors");
 
 const isActive = async (req, res, next) => {
   try {
+    const user = req.user;
+
+    if (user.role == "driver") {
+    }
 
     next();
   } catch (error) {
