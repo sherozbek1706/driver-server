532dbf38d94455973ffc9fb68be14ef97bde71aa
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-07 17:22:47 +0500

    agar anzil topilmasa xatolik beradi.

--- src/modules/order/services/_add.js
+++ src/modules/order/services/_add.js
@@ -4,6 +4,11 @@ const { NotFoundError } = require("../../../shared/errors");
 const add = async ({ body, user }) => {
   const { address_id } = body;
 
+  const existed = await db("address").where({ id: address_id }).first();
+
+  if (!existed) {
+    throw new NotFoundError("Manzil topilmadi!");
+  }
 
   return db("order")
     .insert({ ...body, admin_id: user.id })
