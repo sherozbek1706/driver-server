0b9e01d04a58ca5f4849a3915b30a98831dd82cd
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-07 10:39:22 +0500

    addressni o'chirish uchun controller

--- src/modules/address/_controller.js
+++ src/modules/address/_controller.js
@@ -31,6 +31,21 @@ const listAddress = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const removeAddress = async (req, res, next) => {
+  try {
+    const result = await remove({ params: req.params });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   addAddress,
 };
