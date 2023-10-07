e4ab7c03b21b0df185f5b8f080e88708a13f9826
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-07 10:39:42 +0500

    addressni tahrirlash uchun controller yasalishi.

--- src/modules/address/_controller.js
+++ src/modules/address/_controller.js
@@ -46,6 +46,24 @@ const removeAddress = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const editAddress = async (req, res, next) => {
+  try {
+    const result = await edit({ params: req.params, body: req.body });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   addAddress,
+  listAddress,
+  removeAddress,
+  editAddress,
 };
