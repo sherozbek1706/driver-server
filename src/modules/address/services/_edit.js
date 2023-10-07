ed4fd0093b66a13294e79757e79b6c15ebb7e774
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-07 10:37:38 +0500

    agar id bo'yicha topilmasa xatolik haqida elon bersin.

--- src/modules/address/services/_edit.js
+++ src/modules/address/services/_edit.js
@@ -2,6 +2,12 @@ const db = require("../../../db");
 const { NotFoundError } = require("../../../shared/errors");
 
 const edit = async ({ params, body }) => {
+  const existing = await db("address").where({ id: params.id }).first();
+
+  if (!existing) {
+    throw new NotFoundError("Manzil topilmadi!");
+  }
+
   return db("address").where({ id: params.id }).update(body).returning("*");
 };
 
