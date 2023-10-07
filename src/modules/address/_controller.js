5b3a21d4253d05769a78ed261f98819a07c548c6
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-07 10:09:15 +0500

    address qo'shish uchun controllerni yasash

---
+++ src/modules/address/_controller.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { add } = require("./services");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const addAddress = async (req, res, next) => {
+  try {
+    const result = await add({ body: req.body });
+    res.status(201).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  addAddress,
+};
