27f1ffcb16ffd17329869fe3cbda8536ca5c45c8
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-07 17:21:09 +0500

    order qo'shish uchun controller

---
+++ src/modules/order/_controller.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { add, list } = require("./services");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const addOrder = async (req, res, next) => {
+  try {
+    const result = await add({ body: req.body, user: req.user });
+    res.status(201).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+module.exports = {
+  addOrder,
+  listOrder,
+};
