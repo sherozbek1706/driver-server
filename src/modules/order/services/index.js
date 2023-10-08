62f1b3a680eb4827d5ea2948b654ca7148976b95
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-08 12:43:18 +0500

    progress'dagi orderlarni service

--- src/modules/order/services/index.js
+++ src/modules/order/services/index.js
@@ -2,10 +2,12 @@ const add = require("./_add");
 const list = require("./_list");
 const opened = require("./_opened");
 const closed = require("./_closed");
+const progress = require("./_progress");
 
 module.exports = {
   add,
   list,
   opened,
   closed,
+  progress,
 };
