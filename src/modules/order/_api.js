0e13ca27d8f5c30b3fec5ca88fe52818664e72a2
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-08 12:04:21 +0500

    yopilgan zakazlarni olish uchun router

--- src/modules/order/_api.js
+++ src/modules/order/_api.js
@@ -2,7 +2,12 @@ const { hasRole, isLoggedIn } = require("../../shared/auth");
 const { driver_img_upload } = require("../../shared/upload");
 const { isActive } = require("../driver/middleware");
 const { isBlock } = require("../admin/middleware");
-const { addOrder, listOrder, openedOrder } = require("./_controller");
+const {
+  addOrder,
+  listOrder,
+  openedOrder,
+  closedOrder,
+} = require("./_controller");
 
 const router = require("express").Router();
 
@@ -16,7 +21,6 @@ const mListOrder = [
   driver_img_upload,
   isLoggedIn,
   isBlock,
-  isActive,
   hasRole(["admin", "super_admin"]),
 ];
 const mOpenedOrder = [
@@ -26,9 +30,16 @@ const mOpenedOrder = [
   isActive,
   hasRole(["admin", "super_admin", "driver"]),
 ];
+const mClosedOrder = [
+  driver_img_upload,
+  isLoggedIn,
+  isBlock,
+  hasRole(["admin", "super_admin"]),
+];
 
 router.post("/order", mAddOrder, addOrder);
 router.get("/order", mListOrder, listOrder);
 router.get("/order/open", mOpenedOrder, openedOrder);
+router.get("/order/close", mClosedOrder, closedOrder);
 
 module.exports = router;
