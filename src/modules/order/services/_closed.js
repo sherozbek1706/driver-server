a36b3656b4071bfbd8d91b3465338120bad95f94
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-08 12:04:38 +0500

    yopilgan zakazlarni olish uchun services

---
+++ src/modules/order/services/_closed.js
@@ -0,0 +1,19 @@
+const db = require("../../../db");
+
+const closed = async () => {
+  return db("order")
+    .where({ open: false })
+    .leftJoin("admin", "order.admin_id", "admin.id")
+    .leftJoin("address", "order.address_id", "address.id")
+    .select(
+      "order.id as id",
+      "order.phone_number",
+      "order.created_at",
+      "order.open",
+      "order.address_id",
+      "address.address",
+      "admin.id as admin_id",
+      "admin.first_name as admin_name"
+    );
+};
+module.exports = closed;
--- src/modules/order/services/index.js
+++ src/modules/order/services/index.js
@@ -1,9 +1,11 @@
 const add = require("./_add");
 const list = require("./_list");
 const opened = require("./_opened");
+const closed = require("./_closed");
 
 module.exports = {
   add,
   list,
   opened,
+  closed,
 };
