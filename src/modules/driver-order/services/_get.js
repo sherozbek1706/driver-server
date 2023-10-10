6c4ced6505e1c4b02749b6ed4ac2df56c42f7792
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-10 17:25:10 +0500

    id bo'yicha qidirish

--- src/modules/driver-order/services/_get.js
+++ src/modules/driver-order/services/_get.js
@@ -2,6 +2,14 @@ const db = require("../../../db");
 const { NotFoundError, ForbiddenError } = require("../../../shared/errors");
 
 const get = async ({ params, user }) => {
+  const existing = await db("order")
+    .where({ id: params.id, status: "open" })
+    .first();
+
+  if (!existing) {
+    throw new NotFoundError("Zakaz topilmadi");
+  }
+
 };
 
 module.exports = get;
