d8e2005338fe6b03ef5b5dd5bccc1e1233f51e35
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-10 17:17:58 +0500

    haydochinig olgan zakazini ko'rish

--- src/modules/driver-order/_controller.js
+++ src/modules/driver-order/_controller.js
@@ -16,3 +16,23 @@ const getDriverOrder = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const listDriverOrder = async (req, res, next) => {
+  try {
+    const result = await list({ user: req.user });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+module.exports = {
+  getDriverOrder,
+  listDriverOrder,
+};
