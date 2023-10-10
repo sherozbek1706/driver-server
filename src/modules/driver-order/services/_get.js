360b509ac447a4e86763744df3a88bc6c7ddbf76
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-10 17:24:39 +0500

    get qilish servicesda

---
+++ src/modules/driver-order/services/_get.js
@@ -0,0 +1,7 @@
+const db = require("../../../db");
+const { NotFoundError, ForbiddenError } = require("../../../shared/errors");
+
+const get = async ({ params, user }) => {
+};
+
+module.exports = get;
