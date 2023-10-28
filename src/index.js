380b9a1b0f9ad0b473a52bfa3034d4142decc089
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-28 10:32:30 +0500

    index js o'zgardi .

--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
+const { app, server } = require("./socketio");
 const express = require("express");
-const cors = require("cors");
 const ip = require("ip");
 const config = require("./shared/config");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// import handleError
-const handleError = require("./shared/errors/handle");
-
-// import router
-const admin_route = require("./modules/admin/_api");
-const carmodel_route = require("./modules/car-model/_api");
-const carregion_route = require("./modules/car-region/_api");
-const car_route = require("./modules/car/_api");
-const driver_route = require("./modules/driver/_api");
-const address_route = require("./modules/address/_api");
-const order_route = require("./modules/order/_api");
-const driverorder_route = require("./modules/driver-order/_api");
-
-// registered router
-app.use(admin_route);
-app.use(carmodel_route);
-app.use(carregion_route);
-app.use(car_route);
-app.use(driver_route);
-app.use(address_route);
-app.use(order_route);
-app.use(driverorder_route);
-
-// registered Error
-app.use(handleError);
-
-app.listen(config.port || 5001, () => {
+server.listen(config.port || 5001, () => {
   console.log(`SERVER HAS BEEN STARTED ON LOCAL PORT ${config.port || 5001}`);
   console.log(
     `SERVER HAS BEEN STARTED ON PORT http://${ip.address()}:${
