05907eeb6ae59664d55511b0c0054e8cb9755bb1
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-28 10:43:41 +0500

    statistikani controllerlari yozilib chiqildi.

---
+++ src/modules/statistics/_controller.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const {
+  topDriver,
+  dailyOrder,
+  dailyPaid,
+  newDrivers,
+  dailyWork,
+} = require("./services");
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const topDriverStatistic = async (req, res, next) => {
+  try {
+    const result = await topDriver();
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const dailyOrderStatistic = async (req, res, next) => {
+  try {
+    const result = await dailyOrder();
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const dailyPaidStatistic = async (req, res, next) => {
+  try {
+    const result = await dailyPaid();
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const dailyWorkStatistic = async (req, res, next) => {
+  try {
+    const result = await dailyWork({ params: req.user });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const newDriversStatistic = async (req, res, next) => {
+  try {
+    const result = await newDrivers();
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  topDriverStatistic,
+  dailyOrderStatistic,
+  dailyPaidStatistic,
+  newDriversStatistic,
+  dailyWorkStatistic,
+};
