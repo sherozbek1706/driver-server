c4d9c61b627db0fc7d3168b1085e8176cc2c26dc
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-28 10:46:47 +0500

    serviceslarni export qilib yuborish.

---
+++ src/modules/statistics/services/index.js
@@ -0,0 +1,13 @@
+const topDriver = require("./_top-driver");
+const dailyOrder = require("./_daily-order");
+const dailyPaid = require("./_daily-paid");
+const newDrivers = require("./_new-drivers");
+const dailyWork = require("./_daily-work");
+
+module.exports = {
+  topDriver,
+  dailyOrder,
+  dailyPaid,
+  newDrivers,
+  dailyWork,
+};
