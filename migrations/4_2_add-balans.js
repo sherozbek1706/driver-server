2d4f75c4fb80ef4a047c38bc7c9a510803a13088
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-28 10:29:06 +0500

    balans qo'shadi edni haydovchi o'ziga

---
+++ migrations/4_2_add-balans.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("driver", (table) => {
+    table.bigInteger("balans").defaultTo(10000);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("driver", (table) => {
+    table.dropColumn("balans");
+  });
+};
