e2aeaf3ca033b3f9b674bac74f6ab3141dfb330c
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-11-02 22:53:47 +0500

    mongodb databasega ulanib oldim va connectionlarni log'ga chiqardim.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const { app, server } = require("./socketio");
 const express = require("express");
 const ip = require("ip");
 const config = require("./shared/config");
+const mongodb = require("./mongodb");
 
+mongodb();
 server.listen(config.port || 5001, () => {
   console.log(`SERVER HAS BEEN STARTED ON LOCAL PORT ${config.port || 5001}`);
   console.log(
