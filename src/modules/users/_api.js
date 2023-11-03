d1bcf60c091f5ab947f320325efa43fb5fbccce0
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-11-03 21:05:35 +0500

    user login qilishi uchun service va controller yozib chiqildi.

--- src/modules/users/_api.js
+++ src/modules/users/_api.js
@@ -4,8 +4,10 @@ const { user_img_upload } = require("../../shared/upload");
 const router = require("express").Router();
 
 const mRegisterUser = [user_img_upload];
+const mLoginUser = [user_img_upload];
 
 router.post("/users/check-code", checkCodeUser);
 router.post("/users/register", mRegisterUser, registerUser);
+router.post("/users/login", mLoginUser, loginUser);
 
 module.exports = router;
--- src/modules/users/_controller.js
+++ src/modules/users/_controller.js
@@ -35,4 +35,19 @@ const registerUser = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+const loginUser = async (req, res, next) => {
+  try {
+    const result = await login({ body: req.body });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = { checkCodeUser, registerUser, loginUser };
