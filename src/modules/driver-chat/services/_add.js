3004d5d90f3c6f129b12446644ae7f6b08673dcd
sherozbek1706 <baxtiyorovsherozbek1111@gmail.com>
2023-12-04 15:56:53 +0500

    Endi hamma message pullik bo'ladi

--- src/modules/driver-chat/services/_add.js
+++ src/modules/driver-chat/services/_add.js
@@ -3,5 +3,11 @@ module.exports = async ({ body, user }) => {
   const { message } = body;
   const { id: driver_id } = user;
 
+  const driver = await db("driver").where({ id: driver_id }).first();
+
+  await db("driver")
+    .where({ id: driver_id })
+    .update({ balans: driver.balans - 50 });
+
   return db("driver-msg").insert({ message, driver_id }).returning("*");
 };
